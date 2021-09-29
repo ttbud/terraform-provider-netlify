defc429766535427a726a6ecd4b06c8208f35295
Sam Burba <github@samburba.com>
2021-09-28 17:01:34 -0700

    Refuse to unpack tars outside of unpack dir

--- internal/provider/untar.go
+++ internal/provider/untar.go
@@ -3,14 +3,20 @@ package provider
 import (
 	"archive/tar"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // Untar extracts a gzipped tarfile into the local filesystem
 // In the event of an error, some files may still be written to disk, so any required cleanup should still take place
 func Untar(dest string, reader io.Reader) error {
+	absDest, err := filepath.Abs(dest)
+	if err != nil {
+		return err
+	}
 	gzipReader, err := gzip.NewReader(reader)
 	if err != nil {
 		return err
@@ -31,7 +37,13 @@ func Untar(dest string, reader io.Reader) error {
 			continue
 		}
 
-		target := filepath.Join(dest, header.Name)
+		target, err := filepath.Abs(filepath.Join(absDest, header.Name))
+		if err != nil {
+			return nil
+		}
+		if !strings.HasPrefix(target, absDest) {
+			return fmt.Errorf("tarball attempting to unpack to %s, which is outside of unpack dir", header.Name)
+		}
 
 		switch header.Typeflag {
 		case tar.TypeDir:
